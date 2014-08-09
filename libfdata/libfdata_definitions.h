6ab7d3b32d5bbe86ef2abd0671c72115294336da
Joachim Metz <joachim.metz@gmail.com>
2014-08-09 13:59:49 +0200

    Worked on vector.

--- libfdata/libfdata_definitions.h
+++ libfdata/libfdata_definitions.h
@@ -33,11 +33,11 @@
  * for local use of libfdata
  */
 #else
-#define LIBFDATA_VERSION						20140601
+#define LIBFDATA_VERSION						20140809
 
 /* The libfdata version string
  */
-#define LIBFDATA_VERSION_STRING						"20140601"
+#define LIBFDATA_VERSION_STRING						"20140809"
 
 /* The library flag definitions
  */
